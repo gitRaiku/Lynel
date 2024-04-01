a53aaaf3b71aa236eb8eb8e7c8a54134b6ed174a
gitRaiku <sebi.pintilie@gmail.com>
2024-04-01 22:43:15 +0300

    Steal

--- Usb/main.v
+++ Usb/main.v
@@ -1,8 +1,17 @@
 `timescale 1ns / 1ps
 
-`include usb.v
+`include "usb.v"
 
+module (
+  input sys_clk,
+  input rst_n,
+  input usb_dplus,
+  input usb_dminus
+);
 
-module ();
+usb_mod(
+  sys_clk, rst_n,
+  usb_dplus, usb_dminus
+) ;
 
 endmodule
--- Usb/usb.v
+++ Usb/usb.v
@@ -1,5 +1,28 @@
-module usb(
-
+module usb_mod(
+  input clk,
+  input rst_n,
+  inout dp,
+  inout dm
 );
+
+/*
+localparam
+    st_idle = 3'b000,
+    st_sync = 3'b101,
+    st_run  = 3'b001,
+    st_eop1 = 3'b010,
+    st_eop2 = 3'b011,
+    st_eop3 = 3'b100,
+    st_crc1 = 3'b110,
+    st_crc2 = 3'b111;
+*/
+
+reg dataparity;
+reg cstate;
+reg[3:0] bitstuf;
+
+
+
+wire 
 
 endmodule
